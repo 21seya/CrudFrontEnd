e02cecd6b0203dc1a614292929bf71629cabb4ab
21seya <wallacenevesk9@outlook.com>
2020-01-22 19:28:39 -0300

    crud feito

---
+++ src/components/navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => (
+    <nav>
+        <div className="nav-wrapper grey darken-4">
+        <a href="/" className="brand-logo center">CRUD com React</a>
+        </div>
+    </nav>
+);
+
+export default NavBar;
